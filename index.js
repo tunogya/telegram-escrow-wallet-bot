4ff54caef13e6a927c419b6a39c7cc62b17c8229
tunogya <tunogya@qq.com>
2022-08-20 16:53:23 +0800

    update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ Join our channel (https://t.me/wizardingpay) to receive news about the crypto ma
 
 const editReplyL1MenuContent = async (ctx) => {
   await ctx.answerCbQuery()
-  await ctx.editedMessage(`
+  await ctx.editMessageText(`
 Buy, send, and exchange crypto with @WizardingPayBot. It is always available in your Telegram or Discord account!
 
 Join our channel (https://t.me/wizardingpay) to receive news about the crypto market and @WizardingPayBot updates.
@@ -41,32 +41,28 @@ bot.command('menu', replyL1MenuContent)
 bot.action('backToL1MenuContent', editReplyL1MenuContent)
 
 // L2 Wallet
-const replyL2WalletMenuContent = (ctx) => ctx.reply(`*💰 My Wallet*
+const replyL2WalletMenuContent = (ctx) => ctx.reply(`💰 My Wallet
 
-BTC: 0.00`, {
-  parse_mode: 'MarkdownV2',
-  ...Markup.inlineKeyboard([
-    [Markup.button.callback('Deposit', 'deposit'), Markup.button.callback('Withdraw', 'withdraw')],
-    [Markup.button.callback('Buy crypto with bank card', 'buy_crypto_with_bank_card')],
-    [Markup.button.callback('Exchange', 'exchange')],
-    [Markup.button.callback('« Back', 'backToL1MenuContent')]
-  ])
-})
-
-const editReplyL2WalletMenuContent = async (ctx) => {
-  await ctx.answerCbQuery()
-  await ctx.editedMessage(`
-*💰 My Wallet*
-
-BTC: 0.00`, {
-    parse_mode: 'MarkdownV2',
-    ...Markup.inlineKeyboard([
+BTC: 0.00`, Markup.inlineKeyboard([
       [Markup.button.callback('Deposit', 'deposit'), Markup.button.callback('Withdraw', 'withdraw')],
       [Markup.button.callback('Buy crypto with bank card', 'buy_crypto_with_bank_card')],
       [Markup.button.callback('Exchange', 'exchange')],
       [Markup.button.callback('« Back', 'backToL1MenuContent')]
     ])
-  })
+)
+
+const editReplyL2WalletMenuContent = async (ctx) => {
+  await ctx.answerCbQuery()
+  await ctx.editMessageText(`
+💰 My Wallet
+
+BTC: 0.00`, Markup.inlineKeyboard([
+        [Markup.button.callback('Deposit', 'deposit'), Markup.button.callback('Withdraw', 'withdraw')],
+        [Markup.button.callback('Buy crypto with bank card', 'buy_crypto_with_bank_card')],
+        [Markup.button.callback('Exchange', 'exchange')],
+        [Markup.button.callback('« Back', 'backToL1MenuContent')]
+      ])
+  )
 }
 
 bot.command('wallet', replyL2WalletMenuContent)
@@ -77,7 +73,7 @@ bot.action('backToL2WalletMenuContent', editReplyL2WalletMenuContent)
 const replyL2ChequesMenuContent = (ctx) => ctx.reply(`Sorry, all bot operations are unavailable for your region.`)
 
 const editReplyL2ChequesMenuContent = async (ctx) => {
-  await ctx.editedMessage(`Sorry, all bot operations are unavailable for your region.`)
+  await ctx.editMessageText(`Sorry, all bot operations are unavailable for your region.`)
 }
 
 bot.command('cheques', replyL2ChequesMenuContent)
@@ -86,22 +82,18 @@ bot.action('backToL2ChequesMenuContent', editReplyL2ChequesMenuContent)
 
 // L2 Exchange
 const replyL2ExchangeMenuContent = async (ctx) => {
-  await ctx.reply(`Exchange`, {
-    parse_mode: 'MarkdownV2',
-    ...Markup.inlineKeyboard([
-      [Markup.button.callback('« Back', 'backToL2WalletMenuContent')]
-    ])
-  })
+  await ctx.reply(`Exchange`, Markup.inlineKeyboard([
+        [Markup.button.callback('« Back', 'backToL1MenuContent')]
+      ])
+  )
 }
 
 const editReplyL2ExchangeMenuContent = async (ctx) => {
   await ctx.answerCbQuery()
-  await ctx.editedMessage(`Exchange`, {
-    parse_mode: 'MarkdownV2',
-    ...Markup.inlineKeyboard([
-      [Markup.button.callback('« Back', 'backToL2WalletMenuContent')]
-    ])
-  })
+  await ctx.editMessageText(`Exchange`, Markup.inlineKeyboard([
+        [Markup.button.callback('« Back', 'backToL1MenuContent')]
+      ])
+  )
 }
 
 bot.command('exchange', replyL2ExchangeMenuContent)
@@ -110,33 +102,27 @@ bot.action('backToL2ExchangeMenuContent', editReplyL2ExchangeMenuContent)
 
 // L2 Settings
 const replyL2SettingsMenuContent = async (ctx) => {
-  await ctx.reply(`*⚙️ Settings*
+  await ctx.reply(`⚙️ Settings
 
 Language: 🇬🇧 English
-Local currency: USD`, {
-        parse_mode: 'MarkdownV2',
-        ...Markup.inlineKeyboard([
-          [Markup.button.callback('Change language', 'set_language')],
-          [Markup.button.callback('Change local currency', 'set_local_currency')],
-          [Markup.button.callback('« Back', 'backToL1MenuContent')]
-        ])
-      }
+Local currency: USD`, Markup.inlineKeyboard([
+        [Markup.button.callback('Change language', 'set_language')],
+        [Markup.button.callback('Change local currency', 'set_local_currency')],
+        [Markup.button.callback('« Back', 'backToL1MenuContent')]
+      ])
   )
 }
 
 const editReplyL2SettingsMenuContent = async (ctx) => {
   await ctx.answerCbQuery()
-  await ctx.editedMessage(`*⚙️ Settings*
+  await ctx.editMessageText(`⚙️ Settings
 
 Language: 🇬🇧 English
-Local currency: USD`, {
-        parse_mode: 'MarkdownV2',
-        ...Markup.inlineKeyboard([
-          [Markup.button.callback('Change language', 'set_language')],
-          [Markup.button.callback('Change local currency', 'set_local_currency')],
-          [Markup.button.callback('« Back', 'backToL1MenuContent')]
-        ])
-      }
+Local currency: USD`, Markup.inlineKeyboard([
+        [Markup.button.callback('Change language', 'set_language')],
+        [Markup.button.callback('Change local currency', 'set_local_currency')],
+        [Markup.button.callback('« Back', 'backToL1MenuContent')]
+      ])
   )
 }
 
