a380d52ddbbee65d5052b3ae83bb0f7a52a48159
tunogya <tunogya@qq.com>
2022-08-26 13:13:19 +0800

    delete dynamodb code

--- test/createEthAccount.js
+++
@@ -1,7 +0,0 @@
-const {ethers} = require("ethers")
-
-const privateKey = ethers.utils.randomBytes(32);
-const wallet = new ethers.Wallet(privateKey);
-let keyNumber = ethers.BigNumber.from(privateKey);
-console.log('私钥: ', keyNumber._hex);
-console.log("账号地址: " + wallet.address);
