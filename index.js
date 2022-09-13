7bb65e37f20e90c79e4aa2e4938735cc9b0b02a4
tunogya <tunogya@qq.com>
2022-09-13 11:59:05 +0800

    update

--- index.js
+++ index.js
@@ -48,19 +48,6 @@ Join our channel (https://t.me/wizardingpay) to receive news about updates.
   )
 })
 
-// menu command
-bot.command('menu', async (ctx) => {
-  await ctx.reply(`
-@WizardingPayBot is a log-free escrow wallet that supports use in various social software such as Telegram or Discord.
-
-Join our channel (https://t.me/wizardingpay) to receive news about updates.
-`, Markup.inlineKeyboard([
-        [Markup.button.callback('💰 My Wallet', 'my_wallet')],
-        [Markup.button.url('Support', 'https://www.wakanda-labs.com')]
-      ])
-  )
-})
-
 // back to menu
 bot.action('menu', async (ctx) => {
   await ctx.answerCbQuery()
@@ -72,23 +59,6 @@ Join our channel (https://t.me/wizardingpay) to receive news about updates.
         [Markup.button.callback('💰 My Wallet', 'my_wallet')],
         [Markup.button.url('Support', 'https://www.wakanda-labs.com')]
       ])
-  )
-})
-
-bot.command('wallet', async (ctx) => {
-  const address = ownedAccountBy(ctx.from.id).address
-  ctx.reply(`
-*💰 My Wallet*
-
-ETH: ${address}`,
-      {
-        parse_mode: 'Markdown',
-        ...Markup.inlineKeyboard([
-          [Markup.button.callback('➕ Deposit', 'deposit'), Markup.button.callback('➖ Withdraw', 'choice_withdraw_network')],
-          [Markup.button.callback('🎫 Cheques', 'cheques'), Markup.button.callback('💎 Prize', 'prize')],
-          [Markup.button.callback('« Back', 'menu')]
-        ])
-      }
   )
 })
 
@@ -110,16 +80,31 @@ ETH: ${address}`,
   )
 })
 
-bot.command('cheques', (ctx) => ctx.reply(`Sorry, all bot operations are unavailable for your region.`))
 bot.action('cheques', async (ctx) => {
   await ctx.editMessageText(`Sorry, all bot operations are unavailable for your region.`)
 })
 
-bot.action('exchange', async (ctx) => {
-  await ctx.reply(`Exchange`, Markup.inlineKeyboard([
-        [Markup.button.callback('« Back', 'menu')]
-      ])
-  )
+// 网络，筛选活跃的网络
+// 代币，筛选活跃的代币
+// 金额，输入自定义金额，或者预定义按钮
+// 对象，输入聊天对象
+bot.action('prize', async (ctx) => {
+  // 加载活跃的网络
+  
+  // 返回活跃网络的内联按钮
+  
+})
+
+bot.action(/prize_network_(eth|bsc)/, async (ctx) => {
+  // 确认网络信息到 session
+  // 根据网络信息加载代币数据
+  // 选择代币
+})
+
+bot.action(/prize_token_(eth|bsc)/, async (ctx) => {
+  // 确认代币信息到 session
+  // 输入金额
+  
 })
 
 bot.action('deposit', async (ctx) => {
@@ -135,6 +120,7 @@ You can deposit crypto to this address. Use /depositqrcode to get QR code.
       {
         parse_mode: 'Markdown',
         ...Markup.inlineKeyboard([
+              [Markup.button.callback('QR Code', 'deposit_qrcode')],
               [Markup.button.callback('« Back', 'my_wallet')]
             ]
         )
@@ -142,11 +128,15 @@ You can deposit crypto to this address. Use /depositqrcode to get QR code.
   )
 })
 
-bot.command('depositqrcode', async (ctx) => {
+bot.action('deposit_qrcode', async (ctx) => {
   const address = ownedAccountBy(ctx.from.id).address
+  await ctx.answerCbQuery()
   await ctx.replyWithPhoto(`https://raw.wakanda-labs.com/qrcode?text=${address}`, {
     caption: `*Your WizardingPay deposit address*: ${address}`,
-    parse_mode: 'Markdown'
+    parse_mode: 'Markdown',
+    ...Markup.inlineKeyboard([
+      [Markup.button.callback('« Back', 'my_wallet')]
+    ])
   })
 })
 
@@ -187,41 +177,6 @@ Set to your Google Authenticator and send me current code to submit config.`, {
   await ctx.answerCbQuery()
   await ctx.editMessageText(`Please enter your 2FA code:`, Markup.inlineKeyboard([
     [Markup.button.callback('« Back', 'my_wallet')]
-  ]))
-})
-
-bot.command('withdraw', async (ctx) => {
-  const queryUserRes = await ddbDocClient.send(new QueryCommand({
-    ExpressionAttributeNames: {'#u': 'user_id', '#c': 'category'},
-    TableName: 'wizardingpay',
-    IndexName: 'user-index',
-    KeyConditionExpression: '#u = :u and #c = :c',
-    ExpressionAttributeValues: {
-      ':u': ctx.from.id,
-      ':c': 'telegram'
-    },
-  })).catch(() => {
-  
-  });
-  if (queryUserRes.Count === 0) {
-    const newSecret = twofactor.generateSecret({name: "WizardingPay", account: 'telegram:' + ctx.from.username});
-    ctx.session = {...ctx.session, newSecret: newSecret, intent: 'first-2fa'}
-    await ctx.replyWithPhoto(newSecret.qr, {
-      caption: `You have not set up 2FA. Please scan the QR code to set up 2FA.
-      
-*Your WizardingPay 2FA secret*: ${newSecret.secret}.
-Set to your Google Authenticator and send me current code to submit config.`,
-      parse_mode: 'Markdown',
-      ...Markup.inlineKeyboard([
-        [Markup.button.callback('« Back', 'my_wallet')]
-      ])
-    })
-    return
-  }
-  const secret = queryUserRes.Items[0].secret
-  ctx.session = {...ctx.session, secret: secret, intent: 'verify-2fa-withdraw'}
-  await ctx.reply(`Please enter your 2FA code:`, Markup.inlineKeyboard([
-    [Markup.button.callback('« Back', 'menu')]
   ]))
 })
 
